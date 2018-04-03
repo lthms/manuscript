cebc062f6b82c24135ff98c40be2c02a530d6081
Thomas Letan <thomas.letan@ssi.gouv.fr>
2018-04-03 11:34:44 +0200

    add a convenient script to create pdf diffs

---
+++ diff.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/sh
+
+function clone () {
+    dir="${1}"
+    cwd="${2}"
+    tag="${3}"
+
+    cd "${dir}"
+
+    git clone "${cwd}" .
+    git checkout "${tag}"
+
+    cd "${cwd}"
+}
+
+function diff () {
+    dir="${1}"
+    tag="${2}"
+    target="diff-${tag}"
+
+    latexdiff --flatten "${dir}/main.tex" "./main.tex" > "${target}.tex"
+
+    pdflatex "${target}.tex"
+    bibtex "${target}"
+    pdflatex "${target}.tex"
+    pdflatex "${target}.tex"
+
+    rm -f $(ls ${target}* | grep -v pdf)
+
+}
+
+TMP_DIR="$(mktemp --directory)"
+TAG="${1}"
+
+clone "${TMP_DIR}" "$(pwd)" "${TAG}"
+diff "${TMP_DIR}" "${TAG}"
+
+rm -rf "${TMP_DIR}"
